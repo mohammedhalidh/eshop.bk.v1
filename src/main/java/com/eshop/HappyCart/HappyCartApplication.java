170c7fb20f67b537933da1c7736f875cd9fe3a68
mohammedhalidh <124666625+mohammedhalidh@users.noreply.github.com>
2024-03-06 17:57:43 +0530

    first commit

---
+++ src/main/java/com/eshop/HappyCart/HappyCartApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.HappyCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshop/HappyCart/HappyCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.HappyCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
