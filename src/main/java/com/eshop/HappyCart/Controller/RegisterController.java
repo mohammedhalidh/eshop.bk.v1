a675acf0b6a5aadfaeaad80d70ed9306580806be
mohammedhalidh <124666625+mohammedhalidh@users.noreply.github.com>
2024-03-13 12:39:18 +0530

    Backend updated

--- src/main/java/com/eshop/HappyCart/Controller/RegisterController.java
+++ src/main/java/com/eshop/HappyCart/Controller/RegisterController.java
@@ -3,6 +3,7 @@ package com.eshop.HappyCart.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.eshop.HappyCart.Service.RegisterService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class RegisterController {
 
 	@Autowired
